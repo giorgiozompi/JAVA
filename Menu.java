7cd484eb5a90ff2482b29bb6d11f03b1e473e5a3
giorgiozompi <169030492+giorgiozompi@users.noreply.github.com>
2024-05-22 14:56:57 +0200

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
     Piattospeciale piattospeciale = new Piattospeciale();
     System.out.println("Ecco la lista degli ingredienti: ");
     piattospeciale.elencoPubblico();
+    boolean scelta=true;
    
     
     
-   piattospeciale.elenco();
+    
+    piattospeciale.elenco();
+    System.out.println(piattospeciale.getIngredienti2());
+    piattospeciale.somma(piattospeciale.getIngredienti2());
+    System.out.println("il totale è "+ piattospeciale.somma + " $");
+   while(scelta){
+    System.out.println("premere 1 per ordinare un altro piatto, 2 per terminare ");
+    Scanner myObj=new Scanner(System.in);
+    int ing=myObj.nextInt();
+    if(ing==1){
+        piattospeciale.elenco();
    System.out.println(piattospeciale.getIngredienti2());
    piattospeciale.somma(piattospeciale.getIngredienti2());
    System.out.println("il totale è "+ piattospeciale.somma + " $");
+
+    }
+    else{
+       scelta=false; 
+    }
     }
     
-}
+}}
